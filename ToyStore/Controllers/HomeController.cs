3394276c3bd1f5e9c0a5d47d6884bb70141415eb
WINCTRL-PCSNJK9\Debojyoti <debojyoti1112@gmail.com>
2018-06-28 20:52:30 +0530

    first commit

---
+++ ToyStore/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToyStore.Models;
+namespace ToyStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ToyStoreDBEntities _dataModel = new ToyStoreDBEntities();
+        public ActionResult Index()
+        {
+            return View(_dataModel.Products.ToList());            
+            //return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
